b55907ab40cddbc16f9c7a39a384fb9bbfd9cb65
Geoff Brost <Geoff.Brost@gmail.com>
2020-02-28 15:51:53 -0700

    test git push from VS code

--- react-02/src/components/Cities/fetch.test.js
+++ react-02/src/components/Cities/fetch.test.js
@@ -64,15 +64,15 @@ test('test that the fetch works?', async () => {
 
 
 async function postData(url = '', data = {}) {
-    // Default options are marked with *
+//     // Default options are marked with *
     const response = await fetch(url, {
         method: 'POST',     // *GET, POST, PUT, DELETE, etc.
         mode: 'cors',       // no-cors, *cors, same-origin
         cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
         headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: 'follow',         // manual, *follow, error
         referrer: 'no-referrer',    // no-referrer, *client
--- react-02/src/components/Cities/fetchcities.js
+++ react-02/src/components/Cities/fetchcities.js
@@ -1,10 +1,24 @@
-const url = 'http://localhost:5000/';
+const url = 'https://jsonplaceholder.typicode.com/users';
+const home = 'http://localhost:5000/all';
 
-function getText() {
-    fetch(url)
-    .then(function(res){
-        console.log('first call',res)
-    })   
-}
+async function getText() {
+    fetch(home)
+    .then(response => {
+        console.log(response);
+    let data = response.json()
+    console.log(data)
+    }).catch(e => {
+        console.log(e);
+    }); 
+};
 
-export default getText ;
+async function postTest(url='',data ={}){
+    const res = await fetch((home) ,{
+        method:'POST',
+        body:'Hello World',
+        mode:'cors'
+    })
+
+    
+};
+export {getText, postTest} ;
