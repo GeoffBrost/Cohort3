facdc9e71dc6b54049cc97d512bbca66a608e739
Geoff Brost <Geoff.Brost@gmail.com>
2020-03-03 16:04:04 -0700

    facebook add logout

--- react-02/src/components/facebook/Facebook.js
+++ react-02/src/components/facebook/Facebook.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
 require('dotenv').config()
 
+
 class Facebook extends Component {
     state = {
         isLoggedIn: false,
@@ -12,16 +13,28 @@ class Facebook extends Component {
     responseFacebook = response => {
         this.setState({
             isLoggedIn:true,
-            userID:response.name,
+            name:response.name,
             email:response.email
+        
         })
+        
     }
-
-
+    
+    
+    
     compoenntClicked = () => {
         console.log('clicked')
     }
-
+    logOut =() =>{
+        this.setState({
+            isLoggedIn:false,
+            userID:'',
+            name:'',
+            email:''
+        })
+    FB.logout()
+    }
+    
     render() {
         let fbContent;
         if (this.state.isLoggedIn) {
@@ -32,19 +45,21 @@ class Facebook extends Component {
                     background:'#f4f4f4',
                     padding:'20px'
                 }}>
-                <h2>Hello {this.state.name}</h2> 
+                <h2>Hello {this.state.name}</h2>
+                <button onClick={this.logOut}>Sign Out</button>
                 </div>
             );
         } else {
+            
             fbContent = (<FacebookLogin
-                appId= 'FACEBOOK_ID'
+                appId= '187935692437403'
                 autoLoad={true}
                 fields="name,email"
                 onClick={this.componentClicked}
                 callback={this.responseFacebook} />  
             );
             }
-
+            
         return (
             <div>
                 {fbContent}
